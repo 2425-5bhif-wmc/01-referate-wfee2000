529ada8179f8e40c342098e7e0aaf215c33fab19
wfee2000 <ilmingwinnie19@gmail.com>
2025-03-03 00:36:36 +0100

    feat(java-stub): open bidirectional grpc stream

--- java/chat/stub/src/main/java/at/htl/grpc/chat/ChatStub.java
+++ java/chat/stub/src/main/java/at/htl/grpc/chat/ChatStub.java
@@ -3,6 +3,7 @@ package at.htl.grpc.chat;
 import io.grpc.Grpc;
 import io.grpc.InsecureChannelCredentials;
 import io.grpc.ManagedChannel;
+import io.grpc.stub.StreamObserver;
 
 public class ChatStub {
     public static void main(String[] args) {
@@ -11,5 +12,24 @@ public class ChatStub {
                 .build();
         ChatServiceGrpc.ChatServiceBlockingStub messageStub = ChatServiceGrpc.newBlockingStub(channel);
         String token = messageStub.claimName(Chat.ClaimNameRequest.newBuilder().setName("Winnie").build()).getToken();
+
+        ChatServiceGrpc.ChatServiceStub chatStub = ChatServiceGrpc.newStub(channel);
+        StreamObserver<Chat.OutgoingMessage> outgoingObserver = chatStub
+                .withCallCredentials(new AuthorizationCallCredentials(token))
+                .connect(new StreamObserver<>() {
+                    @Override
+                    public void onNext(Chat.IncomingMessage incomingMessage) {
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                        // should never happen
+                    }
+                });
     }
 }
