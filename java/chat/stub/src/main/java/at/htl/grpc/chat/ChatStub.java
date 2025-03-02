6fe8cbe1fb68eed156cc50cdb8b10538f73251f0
wfee2000 <ilmingwinnie19@gmail.com>
2025-03-03 00:35:08 +0100

    feat(java-stub): get claim for user name

--- java/chat/stub/src/main/java/at/htl/grpc/chat/ChatStub.java
+++ java/chat/stub/src/main/java/at/htl/grpc/chat/ChatStub.java
@@ -1,6 +1,15 @@
 package at.htl.grpc.chat;
 
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+
 public class ChatStub {
     public static void main(String[] args) {
+        ManagedChannel channel = Grpc
+                .newChannelBuilder("localhost:5555", InsecureChannelCredentials.create())
+                .build();
+        ChatServiceGrpc.ChatServiceBlockingStub messageStub = ChatServiceGrpc.newBlockingStub(channel);
+        String token = messageStub.claimName(Chat.ClaimNameRequest.newBuilder().setName("Winnie").build()).getToken();
     }
 }
